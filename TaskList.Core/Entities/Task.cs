d5ade87f45cf25d8ed8da1f926072393fc35fc57
Boris Lipschitz <borislipschitz@hotmail.com>
2012-06-09 18:08:45 +1000

    added packages

---
+++ TaskList.Core/Entities/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskList.Core.Entities
+{
+    public enum Priority
+    {
+        Normal,
+        Low,
+        High
+    }
+
+    public class Task
+    {
+        public int RecordId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public Priority Priority { get; set; }
+    }
+}
